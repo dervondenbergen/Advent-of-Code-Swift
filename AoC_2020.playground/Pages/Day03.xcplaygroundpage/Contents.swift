fe780cf28d3197e5d1b4917cb0d4687e4e31e6c0
Felix De Montis <felix@demont.is>
2021-11-06 21:18:16 +0100

    2020 Day 03

---
+++ AoC_2020.playground/Pages/Day03.xcplaygroundpage/Contents.swift
@@ -0,0 +1,100 @@
+/// Day 3: Toboggan Trajectory
+
+import Foundation
+
+let exampleSlope = """
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#
+"""
+
+let numberSlope = """
+1234
+5678
+abcd
+efgh
+ijkl
+mnop
+qrst
+uvwx
+ylz!
+"""
+
+let filePath = Bundle.main.path(forResource:"input03", ofType: "txt")
+let contentData = FileManager.default.contents(atPath: filePath!)
+let content = String(data:contentData!, encoding: .utf8)!
+
+let inputSlope = content
+
+func getSlopeValueAt(rowPosition: Int, columnPosition: Int, slope: String) -> Character {
+    let rows = slope.split(separator: "\n")
+    let row = rows[rowPosition - 1]
+    
+    let columnCount = row.count
+    let column = Array(row)
+    let columnIndex = column.index(0, offsetBy: (columnPosition - 1) % columnCount)
+    
+    return column[columnIndex]
+}
+
+func getTreeAmount(slope: String, addRow: Int, addColumn: Int) -> Int {
+    let rowCount = slope.split(separator: "\n").count
+    
+    var currentRow = 1;
+    var currentColumn = 1;
+
+    var openSpaces = 0;
+    var treeSpaces = 0;
+
+    for _ in 1...rowCount {
+        currentRow = currentRow + addRow
+        currentColumn = currentColumn + addColumn
+        
+        if (currentRow <= rowCount) {
+        
+            let value = getSlopeValueAt(rowPosition: currentRow, columnPosition: currentColumn, slope: slope)
+
+            if (value == ".") {
+                openSpaces += 1
+//                print("\(i): open")
+            }
+            if (value == "#") {
+                treeSpaces += 1
+//                print("\(i): tree")
+            }
+            
+        }
+    }
+
+//    print("Open amount: \(openSpaces)")
+//    print("Tree amount: \(treeSpaces)")
+    
+    return treeSpaces
+}
+
+struct Way {
+    let addColumn: Int
+    let addRow: Int
+}
+
+let ways = [
+    Way(addColumn: 1, addRow: 1),
+    Way(addColumn: 3, addRow: 1),
+    Way(addColumn: 5, addRow: 1),
+    Way(addColumn: 7, addRow: 1),
+    Way(addColumn: 1, addRow: 2),
+]
+
+let trees = ways.map({ way in
+    return getTreeAmount(slope: inputSlope, addRow: way.addRow, addColumn: way.addColumn)
+})
+
+trees.reduce(1, *)
